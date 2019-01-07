9afbfaa1acfcc04b39a498ba4b10b2d241a580be
Rory O'Connor <rory@thewhiteroom.com>
2019-01-07 07:40:05 -0800

    created new build (forgot to build before last commit)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-	"node": true,
-	"mocha": true
-    },
-    "globals": {
-	"expect": true
-    },
-    parserOptions: {
-      ecmaVersion: 2017,
-      sourceType: 'module'
-    },
-    "extends": "eslint:recommended",
-    "rules": {
-    	"no-console": 0,
-    	"no-mixed-spaces-and-tabs": 0,
-	"no-useless-escape": 0
-    }
+  env: {
+    browser: true,
+    node: true,
+    mocha: true
+  },
+  globals: {
+    expect: true
+  },
+  parserOptions: {
+    ecmaVersion: 2017,
+    sourceType: "module"
+  },
+  extends: "eslint:recommended",
+  rules: {
+    "no-console": 0,
+    "no-mixed-spaces-and-tabs": 0,
+    "no-useless-escape": 0,
+    "no-cond-assign": 0
+  }
 };
